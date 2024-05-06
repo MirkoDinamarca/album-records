3daf47725b8e6280e7d1022a081d4115c3c18930
Mirko Dinamarca <dinamarcamirko.19@gmail.com>
2024-05-06 17:41:06 -0300

    [UPDATE] Nuevos Albums & Corrección comillas simples

--- src/Components/Album/Album.jsx
+++ src/Components/Album/Album.jsx
@@ -1,16 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Album = ({ album, onClickDetailsHandler }) => {
     const navigate = useNavigate();
     const { title, image } = album.albums
     return (
-        <div className='relative rounded-sm hover:animate-pulse hover:cursor-pointer transition-all duration-300' style={{ width: 'auto', height: '300px', boxShadow: 'rgb(95 92 92) 16px 18px 5px -9px' }} onClick={() => navigate(`/details/${album.id}`)}>
+        <div className="relative rounded-sm hover:animate-pulse hover:cursor-pointer transition-all duration-300" style={{ width: "auto", height: "300px", boxShadow: "rgb(95 92 92) 16px 18px 5px -9px" }} onClick={() => navigate(`/details/${album.id}`)}>
             <img className="bg-cover w-auto h-full bg-no-repeat rounded-sm bg-gray-400" src={`/img/${image}`} alt="Imagen Album" />
-            <div className='absolute h-16 w-full bottom-0 bg-white rounded-sm'>
-                <div className='flex items-center justify-evenly'>
-                    <div className='flex flex-col gap-2 '>
-                        <p className='text-gray-800'><b>{title}</b></p>
-                        <p className='text-gray-500'>{album.name}</p>
+            <div className="absolute h-16 w-full bottom-0 bg-white rounded-sm">
+                <div className="flex items-center justify-evenly">
+                    <div className="flex flex-col gap-2 text-center">
+                        <p className="text-gray-800"><b>{title}</b></p>
+                        <p className="text-gray-500">{album.name}</p>
                     </div>
                 </div>
             </div>
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,10 +1,12 @@
 import Album from "../Album/Album"
 const List = ({ albums, onClickDetailsHandler }) => {
   return (
-    <section className='grid grid-cols-4 gap-6 items-center'>
-      {albums.map(album => {
-        return <Album key={album.id} album={album} onClickDetailsHandler={onClickDetailsHandler} />
-      })}
+    <section className="grid grid-cols-4 gap-6 items-center">
+      {albums.length ?
+        albums.map(album => {
+          return <Album key={album.id} album={album} onClickDetailsHandler={onClickDetailsHandler} />
+        }) : 
+        <div className="col-span-4 font-bold tracking-wide">No se encontraron resultados</div>}
     </section>
   )
 }
